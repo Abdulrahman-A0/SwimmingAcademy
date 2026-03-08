a060c7e92dc2ac2728836858e50a73ae1391e85b
Abdulrahman-A0 <abdulrahman.ahmed.m98@gmail.com>
2026-03-08 22:31:31 +0200

    Adjust the api error response

---
+++ Shared/ErrorModels/ErrorTypes.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.ErrorModels
+{
+    public static class ErrorTypes
+    {
+        public const string Validation = "validation_error";
+        public const string Unauthorized = "unauthorized";
+        public const string NotFound = "not_found";
+        public const string Conflict = "conflict";
+        public const string ServerError = "server_error";
+        public const string Forbidden = "forbidden";
+    }
+
+}
