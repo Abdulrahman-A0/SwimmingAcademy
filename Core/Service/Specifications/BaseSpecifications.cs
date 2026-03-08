f5e377ff57e1ea977622608b0c8ce491eb1b2e86
Abdulrahman-A0 <abdulrahman.ahmed.m98@gmail.com>
2026-03-08 22:22:44 +0200

    Add BaseSpecifications for querying the database

---
+++ Core/Service/Specifications/BaseSpecifications.cs
@@ -0,0 +1,61 @@
+﻿using Domain.Contracts;
+using Domain.Entities.Base;
+using System.Linq.Expressions;
+
+namespace Service.Specifications
+{
+    public class BaseSpecifications<TEntity, TKey> : ISpecifications<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        #region Where
+        protected BaseSpecifications(Expression<Func<TEntity, bool>>? criteria)
+        {
+            Criteria = criteria;
+        }
+        public Expression<Func<TEntity, bool>>? Criteria { get; private set; }
+        #endregion
+
+        #region Pagination
+        public int Take { get; private set; }
+        public int Skip { get; private set; }
+        public bool IsPaginated { get; private set; }
+
+        protected void ApplyPagination(int pageIndex, int pageSize)
+        {
+            IsPaginated = true;
+            Take = pageSize;
+            Skip = (pageIndex - 1) * pageSize;
+        }
+        #endregion
+
+        #region Include
+        public List<Expression<Func<TEntity, object>>> IncludeExpressions { get; } = new();
+        protected void AddIncludes(Expression<Func<TEntity, object>> includeExpressions)
+        {
+            IncludeExpressions.Add(includeExpressions);
+        }
+        #endregion
+
+
+        #region ThenInclude
+        private readonly List<Func<IQueryable<TEntity>, IQueryable<TEntity>>> thenIncludeExpressions
+            = new();
+
+        public List<Func<IQueryable<TEntity>, IQueryable<TEntity>>> ThenIncludeExpressions => thenIncludeExpressions;
+
+        protected void AddThenIncludes(Func<IQueryable<TEntity>, IQueryable<TEntity>> thenInclude)
+        {
+            thenIncludeExpressions.Add(thenInclude);
+        }
+        #endregion
+
+        #region Sorting
+        public Expression<Func<TEntity, object>> OrderBy { get; private set; }
+
+        public Expression<Func<TEntity, object>> OrderByDescending { get; private set; }
+
+        protected void AddOrderBy(Expression<Func<TEntity, object>> orderByExpression) => OrderBy = orderByExpression;
+        protected void AddOrderByDescending(Expression<Func<TEntity, object>> orderByDescendingExpression) => OrderByDescending = orderByDescendingExpression;
+        #endregion
+
+    }
+}
