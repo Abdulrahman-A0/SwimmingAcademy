bf265c7b9c291355319b46e3f2e7c5dd019b091a
Abdulrahman-A0 <abdulrahman.ahmed.m98@gmail.com>
2026-03-08 22:21:31 +0200

    removed the Role entity

--- Core/Domain/Entities/Identity/ApplicationUser.cs
+++ Core/Domain/Entities/Identity/ApplicationUser.cs
@@ -9,19 +9,9 @@ namespace Domain.Entities.Identity
     {
         public string FullName { get; set; }
 
-        public string Email { get; set; }
-
-        public string PasswordHash { get; set; }
-
-        public string PhoneNumber { get; set; }
-
-        public int RoleId { get; set; }
-
         public bool IsActive { get; set; }
 
         public DateTime CreatedAt { get; set; }
-
-        public Role Role { get; set; }
 
         #region Navigation Property
 
