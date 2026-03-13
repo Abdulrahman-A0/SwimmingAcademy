0bcf02ab7b37904069378e759b220a74338d2163
Abdo Hafez <abdohafez731@gmail.com>
2026-03-14 00:33:12 +0200

    feat: add CoreServiceExtensions

---
+++ SwimmingAcademy/Extensions/CoreServicesExtensions.cs
@@ -0,0 +1,27 @@
+﻿
+using Service;
+using Service.Implementations;
+using ServiceAbstraction.Contracts;
+
+namespace SwimmingAcademy.Extensions
+{
+    public static class CoreServicesExtensions
+    {
+        public static IServiceCollection AddCoreServices(this IServiceCollection services, ConfigurationManager configuration)
+        {
+            services.AddAutoMapper(cfg => { }, typeof(AssemblyReference).Assembly);
+
+
+            services.AddScoped<IServiceManager, ServiceManager>();
+
+
+            services.AddScoped<ILocationService, LocationService>();
+
+            services.AddScoped<Func<ILocationService>>(provider =>
+                () => provider.GetRequiredService<ILocationService>()
+            );
+
+            return services;
+        }
+    }
+}
