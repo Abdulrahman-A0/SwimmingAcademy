61d3636d7a76fac0d9740c0085dbac11ad37c032
Abdo Hafez <abdohafez731@gmail.com>
2026-03-14 00:40:26 +0200

    feat: add ApiController & LocationController

---
+++ Infrastructure/Presentation/Controller/ApiController.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ApiController : ControllerBase
+    {
+
+    }
+}
---
+++ Infrastructure/Presentation/Controller/LocationController.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction.Contracts;
+using Shared.DTOs;
+using Shared.DTOs.LocationModule;
+
+namespace Presentation.Controller
+{
+    public class LocationController(IServiceManager service) : ApiController
+    {
+        /// <summary>
+        /// Creates a new Location in the system.
+        /// </summary>
+        /// <remarks>
+        /// This endpoint allows administrators to create a new academy location.
+        /// 
+        /// Validation Rules:
+        /// - Name cannot be empty
+        /// - Address cannot be empty
+        /// - City cannot be empty
+        /// 
+        /// Example Request:
+        /// {
+        ///   "name": "Downtown Branch",
+        ///   "address": "123 Nile Street",
+        ///   "city": "Cairo"
+        /// }
+        /// </remarks>
+        /// <response code="200">Location created successfully</response>
+        /// <response code="400">Validation error</response>
+        [HttpPost("AddLocation")]
+        [ProducesResponseType(typeof(AddEntityResultDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<AddEntityResultDTO>> AddLocation(AddLocationDTO dto)
+            => Ok(await service.LocationService.AddLocationAsync(dto));
+
+
+    }
+}
