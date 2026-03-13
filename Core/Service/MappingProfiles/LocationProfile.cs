8a8648d43cac840a15dcaeab7d85c7fd8faea41a
Abdo Hafez <abdohafez731@gmail.com>
2026-03-14 00:37:29 +0200

    feat: add LocationProfile

---
+++ Core/Service/MappingProfiles/LocationProfile.cs
@@ -0,0 +1,18 @@
+﻿
+using AutoMapper;
+using Domain.Entities.AcademyStructure;
+using Shared.DTOs.LocationModule;
+
+namespace Service.MappingProfiles
+{
+    public class LocationProfile : Profile
+    {
+        public LocationProfile()
+        {
+            // Add Location
+            CreateMap<AddLocationDTO, Location>()
+                .ForMember(dest => dest.Id,
+                    opt => opt.Ignore());
+        }
+    }
+}
