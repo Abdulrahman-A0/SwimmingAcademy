6160b09aa135ca7a20de8cb3f3bf27c80e63e3b6
Abdo Hafez <abdohafez731@gmail.com>
2026-03-14 00:34:35 +0200

    feat: add LocationValidationException

---
+++ Core/Domain/Exceptions/ValidationExceptions/LocationValidationException.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Exceptions.Abstraction;
+
+
+namespace Domain.Exceptions.ValidationExceptions
+{
+    public class LocationValidationException : ValidationException
+    {
+        public LocationValidationException(
+            IDictionary<string, string[]> errors)
+            : base(errors)
+        {
+            
+        }
+    }
+}
